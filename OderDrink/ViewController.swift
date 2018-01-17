b65acd64ad84f89de4e74491a638aa1e1397e572
freyahu131 <freyahu131@gmail.com>
2018-01-17 10:32:04 +0800

    Initial Commit

---
+++ OderDrink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OderDrink
+//
+//  Created by 胡靜諭 on 2018/1/17.
+//  Copyright © 2018年 胡靜諭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
